20d894ae696589058bbac1fb819d6cc5b48d690b
Alessandro Laudoni <alessandro.laudoni@factor8.ch>
2016-07-20 09:32:35 +0200

    Webpack devTools

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 require('!style!css!less!./style.less')
 //document.write(require("./content.js"));
 
-window.myFunction = function () {
-    require(['./content'], function (cats) {
+window.myFunction = function() {
+    require(['./content'], (cats) => {
         //var content = require('./content');
         console.log('the content result', cats)
-        var newdiv = document.createElement('H1')
+        const newdiv = document.createElement('H1')
         newdiv.appendChild(document.createTextNode(cats.content))
         document.body.appendChild(newdiv)
     })
--- webpack/dev.config.js
+++ webpack/dev.config.js
@@ -3,8 +3,14 @@
 
 const WebpackConfig = require('webpack-config')
 
-module.exports = (new WebpackConfig()).extend('./webpack/base.config.js').merge({
+module.exports = new WebpackConfig().extend('./webpack/base.config.js').merge({
+    devtool: '#cheap-module-eval-source-map',
     output: {
         filename: "bundle.[hash].js"
+    },
+    module: {
+        preLoaders: [
+            {test: /\.js$/, loader: "source-map"}
+        ]
     }
 })
