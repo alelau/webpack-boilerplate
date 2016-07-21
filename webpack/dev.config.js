8bf4262e2d8b562de50ab877813ff932fbebaf4f
Alessandro Laudoni <alessandro.laudoni@factor8.ch>
2016-07-21 09:46:19 +0200

    Webpack     angular lodash as vendor bundle fixed double import of angular

--- webpack/dev.config.js
+++ webpack/dev.config.js
@@ -2,6 +2,7 @@
 'use strict'
 
 const WebpackConfig = require('webpack-config')
+const webpack = require('webpack')
 
 module.exports = new WebpackConfig().extend('./webpack/base.config.js').merge({
     devtool: '#cheap-module-eval-source-map',
@@ -12,5 +13,8 @@ module.exports = new WebpackConfig().extend('./webpack/base.config.js').merge({
         preLoaders: [
             {test: /\.js$/, loader: "source-map"}
         ]
-    }
+    },
+    plugins:[
+        new webpack.optimize.CommonsChunkPlugin({name:"vendor", filename:"vendor.[hash].js"})
+    ]
 })
